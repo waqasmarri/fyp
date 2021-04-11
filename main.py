f551dda84e04df01ce983d4a8417f03863a37d86
ahsaniqbal1317 <57900863+ahsaniqbal1317@users.noreply.github.com>
2021-04-11 15:24:00 +0500

    new ui

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('/index.html')
+   return render_template('/index2.html')
    # return predict_captions_manual('images/download.jpg')
+
+# @app.route('/index2')
+# def home():
+#    return render_template('/index2.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
@@ -29,16 +33,21 @@ def upload_file():
 
 @app.route('/link', methods = ['GET', 'POST'])
 def link():
-	link = request.args.get("link")
-	response = requests.get(link)
-	return link
-	file = open("static/asdasd.jpg", "wb")
-	file.write(response.content)
-	file.close()
-	destination = "static/sample_image.png"
-	data ={"name": destination,"caption": predict_captions_manual(destination)}
-	response = app.response_class(response=json.dumps(data),mimetype='application/json')
-	return response
+    if request.method == 'POST':
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        # print(request.get_json())
+        # print(request.get_data())
+        link = request.get_data()
+        link = link.decode('UTF-8')
+        response = requests.get(link)
+        destination = "static/"+link[8:12]+".jpg"
+        file = open(destination, "wb")
+        file.write(response.content)
+        file.close()
+        data ={"name": destination,"caption": predict_captions_manual(destination)}
+        response = app.response_class(response=json.dumps(data),mimetype='application/json')
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.1.1.1', port=9000)
