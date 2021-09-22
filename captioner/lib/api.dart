8182d6c7ca814b29071ce29d3dded7bd2eb50e08
waqasmarri <waqascape@gmail.com>
2021-09-22 22:03:38 +0500

    added app

---
+++ captioner/lib/api.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+String uploadUrl = "http://192.168.45.175:5000/api";
+String downloadUrl = "http://192.168.45.175:5000/result";
+
+Future getData(String url) async {
+  http.Response response = await http.get(url);
+  return jsonDecode(response.body);
+}
+
+uploadImage(File imageFile, String url) async {
+  String base64Image = base64Encode(imageFile.readAsBytesSync());
+  Response response = await Dio().post(url, data: base64Image);
+  print(response);
+}
